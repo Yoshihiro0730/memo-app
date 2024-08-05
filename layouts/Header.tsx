2ad43aecb0f14e0b752fd35d5c62a4e44d5a9381
Yoshihiro Netsu <yocchi.engineer0103@gmail.com>
2024-08-06 00:05:57 +0900

    fix fail deploy

--- layouts/Header.tsx
+++ layouts/Header.tsx
@@ -1,10 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => {
     return (
         <header className="flex items-center justify-between h-16">
             <div className="flex items-center space-x-4 w-full">
-                <img src="/jira_icon.jpeg" alt="" className="h-10 w-10 object-contain" style={{ maxWidth: 'none', width: '2.5rem', height: '2.5rem' }} /> 
+            <img 
+                src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/jira_icon.jpeg`}
+                alt="Jira Icon" 
+                className="h-10 w-10 object-contain" 
+                style={{ maxWidth: 'none', width: '2.5rem', height: '2.5rem' }} 
+            />
                 <Link href="/" className="flex-grow">
                     <h1 className="text-2xl font-logo text-center text-white">memo app like JIRA</h1>
                 </Link>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/memo-app/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/memo-app' : '',
+    trailingSlash: true,
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+};
 
 export default nextConfig;
